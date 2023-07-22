a460db0c1ef31bf2e39d4f82f3ec73a5353ed0a5
rajesh9368 <123871464+rajesh9368@users.noreply.github.com>
2023-07-22 13:07:59 +0530

    Add files via upload

---
+++ editor.js
@@ -0,0 +1,35 @@
+const blogTitleField = document.querySelector('.title');
+const articleFeild = document.querySelector('.article');
+
+//banner
+const bannerImage = document.querySelector('#banner-upload');
+const banner = document.querySelector(".banner");
+let bannerPath;
+
+const publishBtn = document.querySelector('.publish-btn');
+const uploadInput = document.querySelector('#image-upload');
+
+bannerImage.addEventListener('change', ()=> {
+    uploadImage(bannerImage, "banner");
+})
+
+uploadInput.addEventListener('change', () => {
+    uploadImage(uploadInput,"image");
+})
+
+const uploadImage = (uploadFile, uploadType) => {
+    const [file] = uploadFile.files;
+    if(file && file.type.includes("image")){
+        const formdata = new FormData();
+        formdata.append('image', file);
+
+        fetch('img/', {
+            method: 'post',
+            body: formdata
+        }).then(res => res.json())
+        .then(data => {
+            bannerPath = '${location.origin}/${data}';
+            banner.style.backgroundImage = 'url("${bannerPath}")';
+        }) 
+    }
+}
